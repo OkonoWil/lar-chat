2b21300e7fe236a8bc5dc28151d0fcaf086682b6
Okono Wilfried <okonowilfried@gmail.com>
2025-03-21 12:15:20 +0100

    fix(auth): redirect user function his role

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,17 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::middleware(['auth', 'verified', \App\Http\Middleware\AdminMiddleware::class])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+
+    Route::get('chat', [\App\Http\Controllers\Web\ChatController::class, 'index'])->name('chat.index');
+
+    Route::middleware(['admin'])->group(function () {
+        Route::get('dashboard', function () {
+            return Inertia::render('dashboard');
+        })->name('dashboard');
+    });
 });
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
